78b40e3797dfd06f87c40336ed830523b7fc93f4
Cory <coadams9@gmail.com>
2019-11-18 16:18:48 -0500

    learning node

--- routes.js
+++ routes.js
@@ -6,6 +6,20 @@ const requestHandler = (req, res) => {
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html')
         res.write(html)
+        res.end()
+    }
+    if (url === '/create-user') {
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            console.log(parsedBody.split('=')[1])
+        })
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        res.end()
     }
 }
 
