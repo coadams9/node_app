a473f004ca2acba8ec2bca0b5f4842c80254c4ce
Cory <coadams9@gmail.com>
2019-11-22 18:39:50 -0500

    serving files

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ const bodyParser = require('body-parser')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const errorPage = require('./routes/errorPage')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
-
-app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found!!</h1>')
-})
+app.use(errorPage)
 
 
 app.listen(3000)
--- routes/errorPage.js
+++ routes/errorPage.js
@@ -1,11 +1,10 @@
 const path = require('path')
-
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
-})
 
+router.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '../', 'views', 'error-page.html'))
+})
 
 module.exports = router
